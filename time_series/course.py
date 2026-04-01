edf2a7d0c48cdf286f7cadc9243a856d846aa3e8
Arthur TIABOU <arthurtiabou@yahoo.com>
2026-04-01 18:29:16 +0200

    (feat): Mise en place de la stratégie de la tortue.

--- time_series/course.py
+++ time_series/course.py
@@ -1,5 +1,32 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-bitcoin = pd.read_csv('./dataset/bitcoin.csv', index_col = 'Date', parse_dates = True) # lecture d'un fichier CSV contenant des données sur le prix du bitcoin
-print(bitcoin.head()) # affichage des 5 premières lignes du DataFrame
+bitcoin = pd.read_csv('./dataset/bitcoin.csv', index_col = 'Date', parse_dates = True) 
+ethereum = pd.read_csv('./dataset/eth.csv', index_col = 'Date', parse_dates = True)
+bitcoin_eth = pd.merge(bitcoin, ethereum, how = 'inner', left_index = True, right_index = True, suffixes = ('_bitcoin', '_ethereum'))
+
+m = bitcoin_eth['Close_bitcoin'].resample('W').agg(['mean', 'std', 'min', 'max']) # resample('W') pour faire une moyenne hebdomadaire du cours du Bitcoin, agg(['mean', 'std', 'min', 'max']) pour calculer la moyenne, l'écart type, le minimum et le maximum du cours du Bitcoin
+# on obtient 4 colonnes : mean, std, min et max
+print(m.head())
+
+rolling = bitcoin_eth['Close_bitcoin'].rolling(window = 30).mean() # rolling(window = 30) pour faire une moyenne mobile sur 30 jours du cours du Bitcoin
+print(rolling.head())
+
+# moyenne mobile exponentielle
+exp_rolling = bitcoin_eth['Close_bitcoin'].ewm(alpha=0.3).mean() # ewm(alpha=0.3) pour faire une moyenne mobile exponentielle sur 30 jours du cours du Bitcoin
+print(exp_rolling.head())
+
+plt.figure(figsize=(12,6))
+plt.subplot(2,1,1)
+bitcoin_eth['Close_bitcoin'].resample('W').mean().plot(label = 'Bitcoin') # resample('W') pour faire une moyenne hebdomadaire du cours du Bitcoin
+plt.subplot(2,1,2)
+bitcoin_eth['Close_ethereum'].resample('W').mean().plot(label = 'Ethereum')
+
+correlation = bitcoin_eth['Close_bitcoin'].corr(bitcoin_eth['Close_ethereum'])
+print(f'La corrélation entre le cours du Bitcoin et de l\'Ethereum est de {correlation:.2f}')
+
+#bitcoin.loc['2017':'2020']['Close'].plot()
+#bitcoin.loc['2017':'2020']['Close'].resample('W').mean().plot()
+#plt.show() # affichage du graphique
---
+++ time_series/exo.py
@@ -0,0 +1,30 @@
+# mettre en place la stratégie de la tortue pour savoir si on doit acheter ou vendre une action
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+bitcoin = pd.read_csv('./dataset/bitcoin.csv', index_col = 'Date', parse_dates = True)
+print(bitcoin['Close'].shape)
+print(bitcoin.head())
+
+bitcoin['Max'] = bitcoin['Close'].rolling(window = 28).max()
+bitcoin['Min'] = bitcoin['Close'].rolling(window = 28).min()
+
+bitcoin['Buy'] = np.zeros(bitcoin.shape[0])
+bitcoin['Sell'] = np.zeros(bitcoin.shape[0])
+
+bitcoin.loc[bitcoin['Close'] > bitcoin['Max'].shift(1), 'Buy'] = 1
+bitcoin.loc[bitcoin['Close'] < bitcoin['Min'].shift(1), 'Sell'] = -1
+
+#on supprime les lignes où il n'y a pas de signal d'achat ou de vente
+bitcoin = bitcoin[(bitcoin['Buy'] != 0) | (bitcoin['Sell'] != 0)]
+
+print(bitcoin.head(30))
+
+plt.figure(figsize=(12,6))
+plt.subplot(2,1,1)
+bitcoin['Close'].plot(label = 'Bitcoin')
+plt.scatter(bitcoin.index, bitcoin['Buy']*bitcoin['Close'], color = 'green', label = 'Buy', marker = '^')
+plt.scatter(bitcoin.index, bitcoin['Sell']*bitcoin['Close'], color = 'red', label = 'Sell', marker = 'v')
+
+plt.show()
